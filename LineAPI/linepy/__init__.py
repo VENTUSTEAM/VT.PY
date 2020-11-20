087ff18661c6232ddc187311d18778462a26dccf
ALBAYRAK <74370405+VENTUSTEAM@users.noreply.github.com>
2020-11-20 18:48:10 +0300

    Create __init__.py

---
+++ LineAPI/linepy/__init__.py
@@ -0,0 +1,13 @@
+from .client import LINE
+from .channel import Channel
+from .oepoll import OEPoll
+from ..akad.ttypes import OpType
+
+__copyright__       = 'Copyright 2018 by Fadhiil Rachman'
+__version__         = '5.1.6'
+__license__         = 'BSD-3-Clause'
+__author__          = 'Fadhiil Rachman'
+__author_email__    = 'ayyztim@gmail.com'
+__url__             = 'https://github.com/VENTUSTEAM/VT.PY'
+
+__all__ = ['LINE', 'Channel', 'OEPoll', 'OpType']
